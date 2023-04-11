f028f7711afe546830ef19767a5ca3cc6dab5127
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-04-11 16:45:32 -0600

    Errores
    Correccion de algunos errores

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-import('/app/index.html')
-    .catch(error => {
-      console.error(`Hubo un error al cargar la página: ${error}`);
-    });
-
-import 'bootstrap/dist/css/bootstrap.css'
+import '/src/app/index.html';
+import 'bootstrap/dist/css/bootstrap.css';
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: './/src/app/index.html',
     polyfills: './src/polyfills.js'
   },
   output: {
@@ -13,8 +13,12 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },      
       {
         test: /\.js$/,
         exclude: /node_modules/,
